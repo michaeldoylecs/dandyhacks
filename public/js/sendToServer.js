73859418f62762754ae304c516dd5e848a643b9c
Bryce Murphy <brshmurphy@gmail.com>
2018-10-27 02:51:32 -0400

    dynamically adds list

--- public/js/sendToServer.js
+++ public/js/sendToServer.js
@@ -1,14 +1,14 @@
 var io = io()
 
 var input = document.getElementById("input")
-var log = document.getElementById("log")
 input.addEventListener("keypress", function(e) {
     if (e.key == "Enter") {
-        console.log(input.value)
         logMsg(input.value)
+        io.emit('send_message', input.value, "Sender", "Recipient")
+        input.value = ""
     }
 })
 
 function logMsg(input) {
-    log.setAttribute("value", input)
+    sendToLog(input)
 }
