5b55b68725d47f9253c15d09d0c8bf69ed0a761c
Bryce Murphy <brshmurphy@gmail.com>
2018-10-27 03:38:48 -0400

    more formatting for text box

--- public/js/sendToServer.js
+++ public/js/sendToServer.js
@@ -2,13 +2,26 @@ var io = io()
 
 var input = document.getElementById("input")
 input.addEventListener("keypress", function(e) {
+    var str = String(input.value).trim()
     if (e.key == "Enter") {
-        logMsg(input.value)
-        io.emit('send_message', input.value, "Sender", "Recipient")
-        input.value = ""
+        if (str != "") {
+            toLog(str)
+            io.emit('send_message', str, "Sender", "Recipient")
+            input.value = ""
+        }
     }
 })
 
-function logMsg(input) {
-    sendToLog(input)
+var length = 0;
+function toLog(text) {
+    var ol = document.getElementById("log_e")
+    var li = document.createElement("li")
+    if (length < 22) {
+        console.log("log set")
+        li.appendChild(document.createTextNode("you: " + text))
+        ol.appendChild(li)
+        length+=1
+    } else {
+        console.log(length)
+    }
 }
