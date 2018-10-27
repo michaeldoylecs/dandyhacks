c1e844ccfbef3ed1d887619e3d48c722e63e8bca
Michael Doyle <mcdoyle7227@gmail.com>
2018-10-26 22:59:52 -0400

    Fix sockets

--- public/js/sendToServer.js
+++ public/js/sendToServer.js
@@ -1,11 +1,9 @@
-var io = io('localhost:3005')
-io.on('connection', function(io) {
-    console.log('a user connected')
-})
+var io = io()
 
 var input = document.getElementById("input")
 input.addEventListener("keypress", function(e) {
     if (e.key == "Enter") {
         console.log(input.value)
+        io.emit('message', input.value)
     }
 })
