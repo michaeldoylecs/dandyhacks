d61c259381d2978c49bf3a6adef3d886209cf0b4
Bryce Murphy <brshmurphy@gmail.com>
2018-10-26 23:52:38 -0400

    *progress*

--- public/js/sendToServer.js
+++ public/js/sendToServer.js
@@ -4,8 +4,14 @@ io.on('connection', function(io) {
 })
 
 var input = document.getElementById("input")
+var log = document.getElementById("log")
 input.addEventListener("keypress", function(e) {
     if (e.key == "Enter") {
         console.log(input.value)
+        logMsg(input.value)
     }
 })
+
+function logMsg(input) {
+    log.setAttribute("value", input)
+}
